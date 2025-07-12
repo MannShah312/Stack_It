87b582a72b9f2a73c3b7cdcebdda8d706d31308b
Mann Shah <Mannshah312@gmail.com>
2025-07-12 15:01:55 +0530

    Comment

---
+++ backend/model/demo.js
@@ -0,0 +1,4 @@
+// Hello 
+// This file is part of the stack-overflow project.
+// It handles the upload of answers to questions.   
+// The code below is responsible for saving the answer data to the database and returning a response to the client.
